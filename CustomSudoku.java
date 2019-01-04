6abbea21dd312cfaa9eba453b63c811ffab3254b
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-04 17:29:49 -0500

    Abstract and constructors added to CSudoku (custom Sudoku)

--- CustomSudoku.java
+++ CustomSudoku.java
@@ -1,3 +1,19 @@
 public class CustomSudoku extends Sudoku{
 
+  public CustomSudoku(){
+
+  }
+
+  public void createpuzzle(){
+
+  }
+
+  public void InputNums(){
+
+  }
+
+  public void answer(){
+
+  }
+
 }
