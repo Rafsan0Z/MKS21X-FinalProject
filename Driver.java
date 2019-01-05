cf9873ca2182c65aa51dede658117da056b13c3e
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-05 16:23:13 -0500

    Mistake and checkAnswer methods added to Driver!

--- Driver.java
+++ Driver.java
@@ -1,7 +1,15 @@
 public class Driver{
 
+  public boolean checkAnswer(){
+    return true;
+  }
+
+  public boolean Mistake(int row, int col){
+    return true;
+  }
+
   public static void main(String[] args){
-    
+
   }
 
 }
