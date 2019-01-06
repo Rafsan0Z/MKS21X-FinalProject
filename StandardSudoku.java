bae18de4bfb68bb1460bdc5142f58f12741a0ea0
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-05 21:29:41 -0500

    private field size removed from StandardSudoku, make changes to prototype accordingly!

--- StandardSudoku.java
+++ StandardSudoku.java
@@ -1,18 +1,17 @@
 public class StandardSudoku extends Sudoku{
 
-  private int size;
   private String[][] puzzle;
 
-  public StandardSudoku(int num){
-    size = num;
+  public StandardSudoku(){
+    int size = 9;
     puzzle = new String[size][size];
     createpuzzle();
     InputNums();
   }
 
   public void createpuzzle(){
-    for(int i = 0; i< size; i++){
-      for(int j = 0; j < size; j++){
+    for(int i = 0; i< 9; i++){
+      for(int j = 0; j < 9; j++){
         puzzle[i][j] = "__";
       }
     }
@@ -28,14 +27,14 @@ public class StandardSudoku extends Sudoku{
 
   public String toString(){
     String result = "--------------------" + "\n";
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < 9; i++){
       result += "|";
-      for(int j = 0; j < size; j++){
+      for(int j = 0; j < 9; j++){
         result += puzzle[i][j];
         if((j+1) % 3 == 0){
           result += "|";
         }
-        if(j == size - 1){
+        if(j == 8){
           result += "\n";
         }
       }
