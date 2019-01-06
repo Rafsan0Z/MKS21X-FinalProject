6a9af29cbf1148e9e3792d411be85c7565d7a764
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-05 21:46:42 -0500

    ArrayList imported and Mistake method updated in Driver!

--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Driver{
 
   private String[][] puzzle;
@@ -8,9 +9,12 @@ public class Driver{
   }
 
   public boolean Mistake(int row, int col){
+    int num = Integer.parseInt(puzzle[row][col]);
     for(int i = 0; i < dim[0]; i++){
+      ArrayList<Integer> row = new ArrayList<Integer>();
       for(int j = 0; j < dim[1]; j++){
-
+        if(i != row && j != col && puzzle[i][j] != "__"){row.add(Integer.parseInt(puzzle[i][j]));}
+        if(row.contains(num)){return false;}
       }
     }
     return true;
