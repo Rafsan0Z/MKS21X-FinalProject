adad6f6bced7f981a64a091ba741fd8be2f988a2
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-01-09 19:55:36 -0500

    add and remove method made abstract in the Sudoku file!

--- Sudoku.java
+++ Sudoku.java
@@ -8,5 +8,7 @@ public abstract class Sudoku{
     Random randgen = new Random();
     return Math.abs(randgen.nextInt()) % 9 + 1;
   }
+  public abstract void add(int row, int col);
+  public abstract void remove(int col, int col);
 
 }
